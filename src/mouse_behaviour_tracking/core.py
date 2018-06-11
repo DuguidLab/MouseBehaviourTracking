2d33e4be64f5ab731c3749c686031cc9815221b5
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-06-11 18:29:08 +0100

    trying stuff with rectangles

--- src/mouse_behaviour_tracking/core.py
+++ src/mouse_behaviour_tracking/core.py
@@ -4,12 +4,21 @@ from kivy.graphics import Color, Rectangle
 
 
 class NoddyWidget(Widget):
-    def on_touch_down(self, touch):
-        print(touch)
+    def __init__(self, *args, **kwargs):
+        super(NoddyWidget, self).__init__()
         with self.canvas:
             Color(1, 1, 0)
             d = 30.
-            Rectangle(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            spos = (self.center_x, self.center_y)
+            self.rect = Rectangle(pos=self.center, size=(d, d))
+
+        self.bind(pos=self.update_rect)
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+
+    def on_touch_down(self, touch):
+        print(touch)
 
 
 class MouseBehaviourTrackingApp(App):

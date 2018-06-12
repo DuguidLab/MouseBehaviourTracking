1b8fa328695c6db611ab4a9e7837f487c22ecdc8
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2018-06-12 14:12:14 +0100

    import metrics package

--- src/mouse_behaviour_tracking/core.py
+++ src/mouse_behaviour_tracking/core.py
@@ -3,6 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle
 from kivy.core.audio import SoundLoader
 from kivy.core.window import Window
+from kivy import metrics
 
 class NoddyWidget(Widget):
     def __init__(self, **kwargs):
